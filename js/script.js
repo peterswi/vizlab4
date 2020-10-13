66fbae20ce99e0185ed3ed174465d7e514063683
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-13 19:55:31 -0400

    building some axes and tossing data on graph

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
         .attr('width', width)
         .attr('height', height)
     
+    //could try to re-work this
     const life=[]
     const income=[]
     var i;
@@ -16,14 +17,23 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
         life[i]=data[i].LifeExpectancy
         income[i]=data[i].Income        
     }
-
+    console.log(data)
     
     const xScale=d3.scaleLinear()
-        .domain([Math.min(... income),Math.max(...income)])
+        .domain(d3.extent(income))
         .range([50,550])
     
     const yScale=d3.scaleLinear()
-        .domain([Math.min(...life), Math.max(...life)])
+        .domain(d3.extent(life))
         .range([50,550])
     
+    svg.selectAll('.chart')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cx',data=>xScale(data.Income))
+        .attr('cy',data=>yScale(data.LifeExpectancy))
+        .attr('r',8)
+        
+
 })
