35cacc718dff0959cebe1e1874effddf4515b166
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-13 19:24:12 -0400

    starting to build some axes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 
+const width = 600;
+const height = 600;
 
 d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
-    console.log('data:',data)
 
+
+    const svg = d3.select('.chart').append('svg')
+        .attr('width', width)
+        .attr('height', height)
+    
+    const life=[]
+    const income=[]
+    var i;
+    for (i=0; i < data.length; i++){
+        life[i]=data[i].LifeExpectancy
+        income[i]=data[i].Income        
+    }
+
+    
+    const xScale=d3.scaleLinear()
+        .domain([Math.min(... income),Math.max(...income)])
+        .range([50,550])
+    
+    const yScale=d3.scaleLinear()
+        .domain([Math.min(...life), Math.max(...life)])
+        .range([50,550])
+    
 })
