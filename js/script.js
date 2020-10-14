ddccd976e715835e64116c5f4685558db859f63c
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-14 16:56:44 -0400

    massive progress

--- js/script.js
+++ js/script.js
@@ -12,22 +12,32 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
 	    .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-  
-    console.log(data)
+    const incomeEx=d3.extent(data, d=>d.Income)
+    
+    incomeEx[0]=incomeEx[0]-3000
+    incomeEx[1]=incomeEx[1]+10000
+
     
     const xScale=d3.scaleLinear()
-        .domain(d3.extent(data, d=>d.Income))
+        .domain(incomeEx)
         .range([0,width])
     
     const xAxis = d3.axisBottom()
-	    .scale(xScale)
+        .scale(xScale)
+        .ticks(5, "s")
+
+    const lifeExpEx=d3.extent(data, d=>d.LifeExpectancy)
+    lifeExpEx[0]=lifeExpEx[0]-2
+    lifeExpEx[1]=lifeExpEx[1]+2
+
     
     const yScale=d3.scaleLinear()
-        .domain(d3.extent(data, d=>d.LifeExpectancy))
+        .domain(lifeExpEx)
         .range([height,0])
     
     const yAxis = d3.axisLeft()
         .scale(yScale)
+        .ticks(10)
 
     const popScale =d3.scaleLinear()
         .domain(d3.extent(data, d=>d.Population))
@@ -35,7 +45,9 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
 
     const popOp =d3.scaleLinear()
         .domain(d3.extent(data, d=>d.Population))
-        .range([0.3,1])
+        .range([0.5,0.3])
+
+    const fillColor=d3.scaleOrdinal(d3.schemeTableau10)
 
     svg.selectAll('.chart')
         .data(data)
@@ -44,15 +56,51 @@ d3.csv('wealth-health-2014.csv', d3.autoType).then(data=>{
         .attr('cx',d=>xScale(d.Income))
         .attr('cy',d=>yScale(d.LifeExpectancy))
         .attr('r',d=>popScale(d.Population))
-        .style('fill', 'purple')
+        .attr('align','center')
+        .style('fill', d=>fillColor(d.Region))
         .style('opacity', d=>popOp(d.Population))
-    
+
+    console.log(d=>fillColor(d.Region))
 
         // Draw the axis
     svg.append("g")
         .attr("class", "axis x-axis")
         .attr("transform", `translate(0, ${height})`)
         .call(xAxis);
-    
 
+    svg.append("g")
+        .attr("class", "axis y-axis")
+        //.attr("transform", `translate(0, ${height})`)
+        .call(yAxis);
+
+    svg.append('text')
+        .attr('class', 'xaxisTitle')
+        .attr('x', xScale(incomeEx[1]-10000)+30)
+        .attr('y', yScale(lifeExpEx[0]+2)+40)
+        .text("Income")
+        .style('text-anchor','middle')
+    svg.append('text')
+        .attr('class','yaxisTitle')
+        .attr("transform", "rotate(-90)")
+        .attr('x', xScale(incomeEx[0]+5000)-120)
+        .attr('y', yScale(lifeExpEx[1]-2)-20)
+        .text("Life Expectancy")
+        .style('text-anchor','middle')
+    svg.append('text')
+        .attr('class','graphTitle')
+        .attr('x',(xScale(incomeEx[0])+xScale(incomeEx[1])/2))
+        .attr('y',yScale(lifeExpEx[1]-2)-30)
+        .text("Health and Wealth of Nations")
+        .style('text-anchor','middle')
+        .style('font-weight','bolder')
+
+    
+    
+    const svgLegend = d3.select('.chart').append('svg')
+        .attr("width", 100)
+        .attr("height", 100)
+	    .append("g")
+
+    svgLegend.selectAll('.chart')
+        .data(fillColor(data.Region))
 })
